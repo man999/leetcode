f6b9b9b31513abf41581075d3edd93b3d42d8b07
man999 <man9992012@ya.ru>
2026-04-15 22:22:23 +0300

    leetCode #2 end

--- src/leetCode.java
+++ src/leetCode.java
@@ -10,29 +10,40 @@ public class leetCode {
 //        System.out.println(Arrays.toString(plusOne(new int[]{9})));
         //#1480
 //        System.out.println(Arrays.toString(runningSum(new int[]{1, 2, 3, 4})));
-        //929
+        //#929
 //        System.out.println(numUniqueEmails(new String[]{
 //                "test.email+alex@leetcode.com",
 //                "test.e.mail+bob.cathy@leetcode.com",
 //                "testemail+david@lee.tcode.com"}
 //        ));
-        //1
+        //#1
 //        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
-        //2
-        LinkedList<Integer> ll = new LinkedList<>();
-        ll.add(2);
-        ll.add(4);
-        ll.add(3);
-//        ListNode l1 = addNode(2, 4, 3);
-//        ListNode l2 = addNode(5, 6, 4);
-//        ListNode res = addTwoNumbers(ListNode l1, ListNode l2);
-//        for (ListNode n : res)
+        //#2
+        ListNode l1 = new ListNode(2);
+        ListNode ln = new ListNode(4);
+        l1.next = ln;
+        ListNode last = ln;
+        ln = new ListNode(3);
+        last.next = ln;
+
+        ListNode l2 = new ListNode(5);
+        ln = new ListNode(6);
+        l2.next = ln;
+        last = ln;
+        ln = new ListNode(4);
+        last.next = ln;
+
+        ListNode res = addTwoNumbers(l1, l2);
+        System.out.println(res.toString());
+        //#
     }
 
+    //--------------------------------------------------
+    //#929
     public static int numUniqueEmails(String[] emails) {
         Set<String> newEmails = new HashSet<>();
         for (String email : emails) {
-            //var1
+            //my solution
 //            String[] subStr = email.split("@");
 //            subStr[0] = subStr[0].replaceAll("\\.", "");
 //            int index = subStr[0].indexOf("+");
@@ -40,7 +51,7 @@ public class leetCode {
 //                subStr[0] = subStr[0].substring(0, index);
 //            }
 //            newEmails.add(subStr[0] + "@" + subStr[1]);
-            //var2
+            //best solution
             int atIndex = email.indexOf("@");
             int plusIndex = email.indexOf("+");
             String str1;
@@ -53,7 +64,9 @@ public class leetCode {
         return newEmails.size();
     }
 
+    //#1480
     public static int[] runningSum(int[] nums) {
+        //my solution
         int[] res = new int[nums.length];
         res[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -62,8 +75,9 @@ public class leetCode {
         return res;
     }
 
+    //#66
     public static int[] plusOne(int[] digits) {
-        //my var
+        //my solution
 //        int step = 1;
 //        for (int i = digits.length - 1; i >= 0; i--) {
 //            int num = digits[i] + step;
@@ -80,7 +94,7 @@ public class leetCode {
 //            digits[0] = step;
 //        }
 //        return digits;
-        //best var
+        //best solution
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] == 9) {
                 digits[i] = 0;
@@ -94,9 +108,10 @@ public class leetCode {
         return digits;
     }
 
+    //#9
     public static boolean isPalindrome(int x) {
         if (x < 0) return false;
-        //my var
+        //my solution
 //        boolean res = true;
 //        char[] chars = Integer.toString(x).toCharArray();
 //        for (int i = 0, j = chars.length-1; i < chars.length-1; i++, j--) {
@@ -106,7 +121,7 @@ public class leetCode {
 //            }
 //        }
 //        return res;
-        //best var
+        //best solution
         int num = x;
         int rev = 0;
         while (num != 0) {
@@ -117,13 +132,14 @@ public class leetCode {
         return rev == x;
     }
 
+    //#1295
     public static int findNumbers(int[] nums) {
         int res = 0;
         for (int num : nums) {
-            //my var
+            //my solution
 //      String str = Integer.toString(num);
 //      if (str.length() % 2 == 0) res++;
-            //best var
+            //best solution
             int rem = 0;
             while (num > 0) {
                 num = num / 10;
@@ -131,13 +147,13 @@ public class leetCode {
             }
             if (rem % 2 == 0) res++;
         }
-        //var2
+
         return res;
     }
 
-    //1
+    //#1
     public static int[] twoSum(int[] nums, int target) {
-        //my var
+        //my solution
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -146,7 +162,7 @@ public class leetCode {
             }
         }
         return null;
-        //best var
+        //best solution
 //        Map<Integer,Integer> map = new HashMap<>();
 //        for(int i=0;i<nums.length;i++){
 //            int compl=target-nums[i];
@@ -158,9 +174,8 @@ public class leetCode {
 //        return null;
     }
 
-    //2
-//     Definition for singly-linked list.
-    public class ListNode {
+    //#2
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -175,10 +190,47 @@ public class leetCode {
             this.val = val;
             this.next = next;
         }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
     }
 
-    //
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+    //#2
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //best solution
+        ListNode sumList = new ListNode(-1);
+        ListNode curr = sumList;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+        int carry = 0;
+
+        while (t1 != null || t2 != null) {
+            int sum = carry;
+            if (t1 != null) {
+                sum = sum + t1.val;
+                t1 = t1.next;
+            }
+            if (t2 != null) {
+                sum = sum + t2.val;
+                t2 = t2.next;
+            }
+
+            ListNode val = new ListNode(sum % 10);
+            curr.next = val;
+            curr = curr.next;
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            ListNode val = new ListNode(carry);
+            curr.next = val;
+        }
+
+        return sumList.next;
     }
 }
